ad49bfee3e43b893bd22e86aeb3c5dc699b67d55
AI1411 <goethe0719@gmail.com>
2021-03-28 23:27:28 +0900

    Update,Deleteメソッド追加

--- routes/routes.go
+++ routes/routes.go
@@ -18,4 +18,6 @@ func Setup(app *fiber.App) {
 	app.Get("/api/users", controllers.AllUsers)
 	app.Get("/api/users/:id", controllers.GetUser)
 	app.Post("/api/users", controllers.CreateUser)
+	app.Put("/api/users/:id", controllers.UpdateUser)
+	app.Delete("/api/users/:id", controllers.DeleteUser)
 }
